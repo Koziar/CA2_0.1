aad19ade7c253daee6f6caf9cfe5105543a8c055
Georgina Thomsen <georgina-thomsen@hotmail.com>
2015-10-07 12:51:29 +0200

    Georgina
    SchemaBuilder class where I populate some tables, and a script for the
    rest of the tables. I f I have time I will randomize them all at some
    point later;)

--- build/generated-sources/ap-source-output/entity/Hobby_.java
+++ build/generated-sources/ap-source-output/entity/Hobby_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-10-06T18:41:49")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-10-07T12:47:13")
 @StaticMetamodel(Hobby.class)
 public class Hobby_ { 
 
